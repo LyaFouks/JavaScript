fd2f9e277fd8b8c1516178b319a18ba6e43f3510
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-17 09:44:18 +0300

    Questionnaire Test (Анкета)

---
+++ tests/questionnaire.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Введите имя: ");
+var lastName = prompt("Введите фамилию: ");
+var age = prompt("Введите возраст: ");
+console.log("Пользователь: " + firstName + " " + lastName);
+console.log("Возраст: " + age);
