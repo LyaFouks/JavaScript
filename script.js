70de536d6dd06794a5ee2400457de604bb06409f
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-17 09:18:52 +0300

    Коментарии и встроенные методы

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
+/* ////////////////////////////////////////////////////////////////////////////
+* Name of user, dsqsssssssqsnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+* adsfdsnqyvqdywvfhqwvbfhqw
+* bdashdbajsdnasd
+* dausbnjkadmgfoasignksa
+*/
 var firstName = "Vasya";
+/*console.log(firstName);
+
+alert(firstName)*/
+
+firstName = prompt("What is your name?");
 console.log(firstName);
-firstName = "Petya";
-console.log(firstName);
-var lastName = "Ivanov";
-console.log(lastName);
-var fullName = firstName + " " + lastName;
-console.log(fullName);
-var age = 16;
-console.log(age);
-
-var isStudent = true;
-console.log(isStudent);
-
-var favouriteColor;
-console.log(favouriteColor);
-
-var player19 = "John";
-player19 = null;
-console.log(player19);
