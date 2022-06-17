377654b867ed4835be324c1ab699613635e10eae
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-17 09:04:10 +0300

    Vars (Переменные)

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("Page Has Loaded!");
+var firstName = "Vasya";
+console.log(firstName);
+firstName = "Petya";
+console.log(firstName);
+var lastName = "Ivanov";
+console.log(lastName);
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+var age = 16;
+console.log(age);
+
+var isStudent = true;
+console.log(isStudent);
+
+var favouriteColor;
+console.log(favouriteColor);
+
+var player19 = "John";
+player19 = null;
+console.log(player19);
