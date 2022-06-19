827e29559eaf537fb87afc357fb7c912c4afdd5e
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-19 19:38:39 +0300

    While test

---
+++ tests/while-test.js
@@ -0,0 +1,21 @@
+var x = 1;
+
+while(x < 30) {
+	console.log(x);
+	x+=2;
+}
+
+var z = 5;
+var y = 1;
+
+while(y <= 6) {
+	console.log(z * y);
+	y++;
+}
+
+var q = 30;
+
+while(q >= 2) {
+	console.log(q);
+	q-=2;
+}
