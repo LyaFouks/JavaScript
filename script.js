2d25ea66b67e0a79b7170fc3290329eff4a64472
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-19 18:35:03 +0300

    Оператор switch

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var userName = "Jana";
-var userWeight = 91;
+var section = "html5";
 
-
-// if(userWeight > 90) {
-// 	console.log(userName + " has redundant weight.");
-// }
-// else {
-// 	console.log(userName + " has normal weight.");
-// }
-
-var weightDescription
-
-weightDescription = userWeight > 90 ? " redundant" : " normal";
-
-userWeight > 90 ? console.log(userName + " has" + weightDescription + "weight.") : console.log(userName + " has" + weightDescription + "weight.");
+switch(section) {
+	case "html" :
+	case "html5" :
+	console.log("Вы изучаете раздел 'HTML'");
+	break;
+	case "css" :
+	console.log("Вы изучаете раздел 'CSS'");
+	break;
+	case "javascript" :
+	console.log("Вы изучаете раздел 'JavaScript'");
+	break;
+	default :
+	console.log("Вы изучаете какой-то раздел");
+}
