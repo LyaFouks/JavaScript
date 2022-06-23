75aa91c3567972f59d9578d72a1ae8a30835a44d
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-23 20:31:03 +0300

    Functions

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-var x = 1;
 
-while(x <= 10) {
-	console.log(x);
-	x++;
+function complexHello(helloText , name, age) {
+	console.log(helloText + "My name is " + name + ". I am " + age + " years old.");
 }
-console.log("Loop has finished.")
 
-var helloString = "Hello JavaScript.";
-var count = 0;
 
-while (count < helloString.length) {
-	console.log(helloString[count]);
-	count++;
+function square(number) {
+	return number * number;
 }
+
+var x= square(2);
+console.log(x);
+
+square(314);
+
+function isSquareBig(side) {
+	var squareArea = square(side);
+	if(squareArea > 100) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+console.log(isSquareBig(2));
+
+// function rectangurlarArea(width , height) {
+// 	console.log(width * height);
+// }
+
+// rectangurlarArea(4,5);
+
+// complexHello("Hey!", "John", 30);
+// complexHello("Hi!", "Joe", 40);
+// complexHello("Hello!", "Joane", 20);
