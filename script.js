774bb8cdcace87b1d281af3b12af11fbc30ccb89
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-24 08:01:53 +0300

    Функции Statements vs Expressions

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
+// function animalVoice(animal, animalName) {
+// 	switch(animal) {
+// 		case "dog" :
+// 		return animalName + " barks.";
+// 		case "cat" :
+// 			return animalName + " meows.";
+// 		case "pig" :
+// 			return animalName + " grunts.";
+// 		default:
+// 			return animalName + " makes some sounds.";
+// 	}
+// }
 
-function complexHello(helloText , name, age) {
-	console.log(helloText + "My name is " + name + ". I am " + age + " years old.");
-}
+// console.log(animalVoice("tiger", "Richard"));
 
 
-function square(number) {
-	return number * number;
-}
-
-var x= square(2);
-console.log(x);
-
-square(314);
-
-function isSquareBig(side) {
-	var squareArea = square(side);
-	if(squareArea > 100) {
-		return true;
-	} else {
-		return false;
+var animalVoice = function(animal, animalName) {
+	switch(animal) {
+		case "dog" :
+		return animalName + " barks.";
+		case "cat" :
+			return animalName + " meows.";
+		case "pig" :
+			return animalName + " grunts.";
+		default:
+			return animalName + " makes some sounds.";
 	}
 }
 
-console.log(isSquareBig(2));
-
-// function rectangurlarArea(width , height) {
-// 	console.log(width * height);
-// }
-
-// rectangurlarArea(4,5);
-
-// complexHello("Hey!", "John", 30);
-// complexHello("Hi!", "Joe", 40);
-// complexHello("Hello!", "Joane", 20);
+console.log(animalVoice("cat", "Richard"));
