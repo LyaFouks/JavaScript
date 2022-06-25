a04af8919a1d7183745784430e2b91c8053c6329
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-25 19:15:17 +0300

    DOM Selectors Test

---
+++ tests/dom-test.js
@@ -0,0 +1,9 @@
+console.log(document.getElementById("this").innerHTML);
+
+var x = document.getElementsByClassName("anotherElements");
+console.log(x[0].innerHTML);
+
+console.log(document.getElementsByTagName("li")[2].innerHTML);
+
+ console.log(document.querySelectorAll("li")[2].innerHTML);
+
