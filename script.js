1da0327e75c6b84520ef1165d87d33bb0e23920e
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-25 20:41:56 +0300

    HTML content change

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// document.getElementById("my").innerHTML = "New Value";
+var ul = document.getElementsByTagName("ul");
+console.log(ul[0].innerHTML);
+console.log(ul[0].textContent);
 
-// var x = document.getElementsByClassName("anotherElements");
-// console.log(x);
-// console.log(x[1].innerHTML);
-
-
-// console.log(document.getElementsByTagName("body")[0]);
-
-// console.log(document.querySelector("p").innerHTML);
-
-// console.log(document.querySelectorAll("li")[1].innerHTML);
-
-// console.log(document.querySelectorAll("#my"));
+var li = document.getElementsByTagName("li");
+console.log(li[0].innerHTML);
+li[0].innerHTML = "The <strong>html</strong> elements as objects";
+console.log(li[0].innerHTML);
+console.log(li[0].textContent);
+li[0].textContent = "The <strong>html</strong> elements as objects";
+console.log(li[0].innerHTML);
