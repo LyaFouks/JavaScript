fb31d33b2e347767fb9c30aa3bbdd1219e6d858f
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-25 19:10:41 +0300

    DOM Selectors

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-var carSeller1 = {
-	fristName: "Jack",
-	lastName: "White",
-	registrationYear: 2016,
-	hasDiscount: true,
-	discount: 0,
-	calculateDiscount: function(year) {
-		var discount;
-		var numbersOfYear = 2019 - this.registrationYear;
-		if(numbersOfYear <= 2) { 
-			discount = 0;
-		}else if((numbersOfYear > 2) && (numbersOfYear <= 5)) {
-			discount = 20;
-		}else if(numbersOfYear > 5) {
-			discount = 30;
-		}
-		this.discount = discount;
+// document.getElementById("my").innerHTML = "New Value";
 
-	}
-}
+// var x = document.getElementsByClassName("anotherElements");
+// console.log(x);
+// console.log(x[1].innerHTML);
 
-carSeller1.calculateDiscount();
 
-console.log(carSeller1);
+// console.log(document.getElementsByTagName("body")[0]);
+
+// console.log(document.querySelector("p").innerHTML);
+
+// console.log(document.querySelectorAll("li")[1].innerHTML);
+
+// console.log(document.querySelectorAll("#my"));
