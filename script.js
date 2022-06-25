91f1295bc32ac195ce814d1440f56d6c6b4a4f41
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-25 15:38:52 +0300

    Objects Methods THIS

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var carSeller1 = {
 	fristName: "Jack",
 	lastName: "White",
-	registrationYear: 2017,
+	registrationYear: 2016,
 	hasDiscount: true,
-	discountCalculation: function(year) {
+	discount: 0,
+	calculateDiscount: function(year) {
 		var discount;
-		var numbersOfYear = 2019 - year;
+		var numbersOfYear = 2019 - this.registrationYear;
 		if(numbersOfYear <= 2) { 
 			discount = 0;
 		}else if((numbersOfYear > 2) && (numbersOfYear <= 5)) {
@@ -13,9 +14,11 @@ var carSeller1 = {
 		}else if(numbersOfYear > 5) {
 			discount = 30;
 		}
-		return discount;
+		this.discount = discount;
 
 	}
 }
 
-console.log(carSeller1.discountCalculation(2017));
+carSeller1.calculateDiscount();
+
+console.log(carSeller1);
