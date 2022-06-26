b620fd1b2c35fdb0aefa5fdfea66ff879e763555
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-26 09:03:03 +0300

    DOM Events

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var image = document.getElementsByTagName("img");
-image[0].getAttribute("src");
-image[0].setAttribute("src" , "images/back4.jpg");
+var liElements = document.querySelectorAll("li");
 
-var link = document.getElementsByTagName("a");
-link[0].getAttribute("href");
-link[0].setAttribute("href" , "http://masters-of-code.com");
+console.log(liElements);
+
+var chanheLiColor = function() {
+		this.style.color = "green";
+	};
+
+for (var i = 0; i < liElements.length; i++) {
+	liElements[i].addEventListener("click", chanheLiColor)
+}
