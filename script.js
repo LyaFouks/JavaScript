280acfd95bd88d1403a77c2d881e3628d0e9cd19
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-06-26 08:43:17 +0300

    DOM изменение атрибутов

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var li3 = document.getElementById("rum");
-li3.classList.add("stylish");
-li3.classList.remove("stylish");
-li3.classList.toggle("stylish");
+var image = document.getElementsByTagName("img");
+image[0].getAttribute("src");
+image[0].setAttribute("src" , "images/back4.jpg");
+
+var link = document.getElementsByTagName("a");
+link[0].getAttribute("href");
+link[0].setAttribute("href" , "http://masters-of-code.com");
